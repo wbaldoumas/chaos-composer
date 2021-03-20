256fde1ffa48c53c8ed664688af6c3181ea21f0f
William Baldoumas <itabaiyu@gmail.com>
2021-03-20 14:13:02 -0400

    Set up CI pipeline

---
+++ tests/ChaosComposer.UnitTests/MockTest.cs
@@ -0,0 +1,15 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace ChaosComposer.UnitTests
+{
+    [TestFixture]
+    public class MockTest
+    {
+        [Test]
+        public void Stub()
+        {
+            true.Should().BeTrue();
+        }
+    }
+}
